4d47d3fa4f4301f8e14c08b734fa9ac0e7115316
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-01-21 23:39:07 +1300

    added wip different enemy types

--- Entities/Enemy/enemy.gd
+++ Entities/Enemy/enemy.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends RigidBody2D
 
 enum EnemyType{
 	STATIC,
@@ -9,18 +9,41 @@ enum EnemyType{
 @onready var sprite = $Sprite2D
 @onready var hurt_area = $HurtArea
 
-@export var enemy_type:EnemyType
+@export var enemy_type:EnemyType = EnemyType.STATIC
 
 
 
 func _ready():
-	hurt_area.collision_occured.connect(die)
+	#hurt_area.collision_occured.connect(die)
+	
+	match enemy_type:
+		EnemyType.ARROW:
+			rotate_and_shoot_in_random_direction()
+		EnemyType.BOUNCER:
+			pass
+		EnemyType.STATIC:
+			pass
+
+
+func rotate_and_shoot_in_random_direction():
+	# set a random rotation for transform
+	rotation_degrees = randf_range(0, 360)
+	var tween = create_tween()
+	tween.tween_property(self, "rotation_degrees", 10, 2)  # Adjust duration as needed
+	tween.finished.connect(
+		func(): 
+			var forward_dir = Vector2.RIGHT.rotated(rotation)  # RIGHT is the default forward
+			linear_velocity = forward_dir * 400  # Adjust speed as desired
+ 
+	)
 
 
 
 
 func die():
+	print_debug("DIE")
 	hurt_area.queue_free()
+	linear_velocity=Vector2.ZERO
 	var tween = create_tween()
 		# Flash the sprite 3 times
 	for i in range(3):
@@ -35,5 +58,13 @@ func die():
 	tween.parallel().tween_property(sprite, "rotation", PI/2, 0.5)
 	tween.parallel().tween_property(sprite, "scale", Vector2.ZERO, 0.5)
 
-	# Connect to the tween's finished signal to queue_free 
-	tween.connect("finished", queue_free)
+	# Connect to the tween's finished signal to queue_free  
+	tween.finished.connect(queue_free)
+
+
+
+func _physics_process(delta):
+	var collision = move_and_collide(linear_velocity * delta)
+	if collision:
+		print(collision)
+		die()
--- Entities/Enemy/hurt_area.gd
+++ Entities/Enemy/hurt_area.gd
@@ -6,5 +6,7 @@ signal collision_occured
 func _on_body_entered(body):
 	if body is Player:
 		body.take_damage()
-	collision_occured.emit()
+		collision_occured.emit()
+	if body.is_in_group("WALLS"):
+		collision_occured.emit()
 	
